7ef2ea1b6d999f8e530a7c8f08c008e9a2e258fa
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-03 20:51:47 +0530

    added backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+#-------------------#
+#terrafrom backend
+#-------------------#
+terraform {
+  backend "s3" {
+    bucket         = "mylaravalappbucket"
+    key            = "mylaravalappbucket/teraform/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "laraval-terraform-state-locking"
+    encrypt        = true
+  }
+}
