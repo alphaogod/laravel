3168cd39870858d4deee1ffc5445b49dee15e991
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-04 22:10:46 +0530

    change

--- eks.tf
+++ eks.tf
@@ -1,26 +1,30 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "17.1.0"
-
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.28"
-  subnets         = module.vpc.private_subnets
+  cluster_version = var.kubernetes_version
+  subnet_ids      = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  tags = {
+    cluster = "demo"
+  }
 
   vpc_id = module.vpc.vpc_id
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 10
-      min_capacity     = 1
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    instance_types         = ["t3.medium"]
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  }
 
-      instance_type = "t3a.medium"
-      key_name      = "laravelnew.pem"
+  eks_managed_node_groups = {
 
-      additional_tags = {
-        Environment = "test"
-        Name        = "eks-worker-node"
-      }
+    node_group = {
+      min_size     = 2
+      max_size     = 6
+      desired_size = 2
     }
   }
 }
