10589d129394741a85ca370a3ab219915c781ee0
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-04 13:13:07 +0530

    change

--- vpc.tf
+++ vpc.tf
@@ -42,3 +42,12 @@ resource "aws_route_table_association" "private_route_association" {
   subnet_id = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
