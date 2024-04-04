2c96ca17e2a57c58ac3ff9c9a4322d24564fe0b1
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-04 21:37:08 +0530

    change

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
+  version = "5.7.0"
 
   name                 = "my-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   enable_nat_gateway   = true
