7de4259fd03cfced82c055e6504c0a79fa60cf9f
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-04 16:27:54 +0530

    change

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "my_eks_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
   vpc_config {
     security_group_ids = [aws_security_group.eks_cluster_sg.id]
-    subnet_ids = subnet_ids = [
+    subnet_ids = [
       aws_subnet.public_subnet_1.id,
       aws_subnet.public_subnet_2.id
     ]
