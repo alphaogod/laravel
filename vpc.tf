69d71bf0c86496044b0b5810140c41e73f796754
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-11 01:05:18 +0530

    change

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   name                 = "my-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  azs                  = ["ap-south-1a", "ap-south-1b"]
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_nat_gateway   = true
