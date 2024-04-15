a8d7b18eaad17c6044e0eb4025a9025ae3d7b191
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-15 13:48:58 +0530

    changes

--- eks.tf
+++ eks.tf
@@ -1,68 +1,19 @@
+# Create an EKS cluster
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
 
-  cluster_name    = "my-cluster"
-  cluster_version = "1.29"
-
-  cluster_endpoint_public_access  = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  #control_plane_subnet_ids = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium", "t3a.medium", "t2.micro", "t2.medium"]
-  }
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.20"  # Replace with your desired EKS version
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
-    example = {
-      min_size     = 1
-      max_size     = 10
-      desired_size = 1
-
-      instance_types = ["t3a.medium"]
-      capacity_type  = "SPOT"
+    my_node_group = {
+      desired_capacity = 2
+      max_capacity     = 5
+      min_capacity     = 1
+      instance_type    = "t3a.medium"
     }
-  }
-
-  # Cluster access entry
-  # To add the current caller identity as an administrator
-  enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::590184096928:role/laravelapp"
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
-        }
-      }
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,29 +1,18 @@
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "my-eks-cluster"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
+# Create a VPC
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 20.0"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  name                 = "my-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["ap-south-1a", "ap-south-1b"]  # Replace with your desired availability zones
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]  # Replace with your desired private subnet CIDRs
+  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]  # Replace with your desired public subnet CIDRs
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Terraform = "true"
     Environment = "dev"
   }
 }
