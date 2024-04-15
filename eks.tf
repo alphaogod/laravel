c8e2be81f8528d14dc376036f0681297b6077602
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-15 14:23:52 +0530

    changes

--- eks.tf
+++ eks.tf
@@ -1,19 +1,26 @@
 # Create an EKS cluster
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 20.0"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.20"  # Replace with your desired EKS version
+  cluster_version = "1.20" 
+  cluster_endpoint_public_access  = true
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
-
+  
+  
   eks_managed_node_groups = {
     my_node_group = {
       desired_capacity = 2
       max_capacity     = 5
       min_capacity     = 1
       instance_type    = "t3a.medium"
+      enable_cluster_creator_admin_permissions = true
     }
   }
 }
+resource "aws_cloudwatch_log_group" "custom_log_group" {
+    name             = "/my-custom-log-group"
+    retention_in_days = 30
