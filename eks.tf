b1a6d3ea76c1d3e883fdf0fe4643f8f2920d92c2
Anoop-v-official <anoopvtvm14@gmail.com>
2024-04-22 15:23:52 +0530

    changes

--- eks.tf
+++ eks.tf
@@ -1,22 +1,47 @@
-# Create an EKS cluster
-
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.29" 
+  cluster_name    = "my-cluster"
+  cluster_version = "1.29"
+
   cluster_endpoint_public_access  = true
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-  
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = "vpc-039fea0023b64fd45"
+  subnet_ids               = ["subnet-0ceffbe2ce6f25a39", "subnet-0e3472b2af17657d8", "subnet-071e6a5b8253b0256"]
+  control_plane_subnet_ids = ["subnet-076d11ee10044c8f2", "subnet-0f41d521da237a275", "subnet-0d4aa90db0764ec2f"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3a.medium", "t3.medium", "t3a.small", "t3.small"]
+  }
+
   eks_managed_node_groups = {
-    my_node_group = {
-      desired_capacity = 2
-      max_capacity     = 5
-      min_capacity     = 1
-      instance_type    = "t3a.medium"
-      enable_cluster_creator_admin_permissions = true
+    example = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
 
   access_entries = {
     # One access entry with a policy associated
@@ -36,9 +61,8 @@ module "eks" {
     }
   }
 
-      authentication_mode = "API_AND_CONFIG_MAP"
-      cluster_endpoint_public_access  = true
-
-    }
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
